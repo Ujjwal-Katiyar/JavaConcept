a98e7cc19ccbc5a079250848c2d2d95caac6b99a
Ujjwal Katiyar <katiyarujjwal122001@gmail.com>
2024-08-16 20:26:21 +0530

    Testing

--- src/Java8/JavaStreams/Streams.java
+++ src/Java8/JavaStreams/Streams.java
@@ -1,16 +1,42 @@
 package Java8.JavaStreams;
 
-interface predicatee
-{
-	public static final int flag=12;
-	
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import Java8.Java.entity.Student;
+import Java8.Java.entity.StudentData;
+
+interface predicatee {
+	public static final int flag = 12;
+
 	public abstract void test();
-	
+
 }
 
 public class Streams {
-	
+
 	public static void main(String[] args) {
+		
+		int[] arr=new int[] {1,2,3,4};
+		
+		//System.out.println(arr);
 		System.out.println("This is the java8 branch");
+		List<Student> student=StudentData.getStudent();
+		
+		
+		List<Integer> l=List.of(1,2,3,4);
+		
+		System.out.println(l);
+		 
+		System.out.println(student);
+		
+	// student.stream().filter(s->s.getFirstName().startsWith("A")).forEachOrdered(System.out::println);;
+	Map<String, List<Student>> collect = student.stream().collect(Collectors.groupingBy(s->s.getDepartmantName()));
+	
+	for( Map.Entry<String, List<Student>> entry : collect.entrySet())
+	{
+		//System.out.println(entry.getKey()+"  "+entry.getValue());
+	}
 	}
 }
---
+++ src/MarkerInterface/MarkerInterfaceTest.java
@@ -0,0 +1,10 @@
+package MarkerInterface;
+
+public class MarkerInterfaceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
